dc0cfdcb7fb3c95524653af0bf5924b273bd044d
M3liorSp3ro <danila.kachinkin.16@gmail.com>
2024-08-01 17:29:10 +0300

    cors

---
+++ cors.js
@@ -0,0 +1,26 @@
+function getEstimates() {
+    fetch(`https://6502dc12a0f2c1f3faeafdf7.mockapi.io/test/estimates/1`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Ошибка запроса: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Отправить данные в родительский фрейм (React-приложение)
+            console.log('data', data);
+            window.parent.postMessage({
+                district: data.district,
+                realtyComplex: data['realty complex'],
+                flate: data.flate
+            }, '*');
+        })
+        .catch(error => {
+            console.error('Ошибка при получении данных:', error);
+        });
+}
+
+
+
+// Вызвать функцию для получения оценок
+getEstimates();
